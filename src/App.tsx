3b54381ce1622de7b5746a3f389cf71d6c05f2da
unknown <phillip@lunenetworks.com>
2024-08-14 19:31:07 -0700

    Added playerIDs that generate on browser load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BoardUI from './components/Board';
 import HUD from './components/HUD';
 import PublicOpinion from './components/PublicOpinion';
 import Scoreboard from './components/Scoreboard';
 import { GameStateProvider, size } from './GameState';
 import { PlayerColor, PollRegion } from './types';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const defaultPollRegion: PollRegion = {
@@ -24,6 +27,23 @@ function App() {
   const [settingPollRegion, setSettingPollRegion] =
     useState<PlayerColor | null>(null);
 
+  const [playerId, setPlayerId] = useState<string | null>(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    let idFromUrl = queryParams.get('playerId');
+
+    if (!idFromUrl) {
+      idFromUrl = uuidv4();
+      queryParams.set('playerId', idFromUrl);
+      navigate({ search: queryParams.toString() }, { replace: true });
+    }
+
+    setPlayerId(idFromUrl);
+  }, [location.search, navigate]);
+
   return (
     <GameStateProvider>
       <div
@@ -41,6 +61,7 @@ function App() {
           {/* Left Side */}
           <div>
             <h1 style={{ paddingBottom: '35px' }}>Campaign</h1>
+            {playerId}
             <BoardUI
               pollInputs={pollInputs}
               setPollInputs={setPollInputs}
