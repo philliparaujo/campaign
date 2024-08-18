9b73776494baf7ba4b692c779b589825b8117c4e
unknown <phillip@lunenetworks.com>
2024-08-18 16:44:05 -0700

    Finished migrating server from webpack to esbuild

--- server/esbuild.config.js
+++ server/esbuild.config.js
@@ -1,11 +1,44 @@
-const { build } = require('esbuild');
+const { build, context } = require('esbuild');
 const path = require('path');
+const { exec } = require('child_process');
 
-build({
+const outfile = path.resolve(__dirname, 'dist', 'bundle.js');
+
+const ctx = context({
   entryPoints: ['./src/index.ts'], // Entry point of your application
   bundle: true, // Bundle all dependencies
   platform: 'node', // Target Node.js (instead of browser)
   target: 'es2020', // Target ECMAScript 2020 to support BigInt
-  outfile: path.resolve(__dirname, 'dist', 'bundle.js'), // Output file
+  outfile: outfile, // Output file
   external: ['node_modules/*'], // Exclude node_modules from the bundle
 }).catch(() => process.exit(1));
+
+function startNode() {
+  const nodeProcess = exec(`node ${outfile}`);
+
+  nodeProcess.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  nodeProcess.stderr.on('data', (data) => {
+    console.error(data);
+  });
+
+  nodeProcess.on('close', (code) => {
+    console.log(`Node.js process exited with code ${code}`);
+  });
+}
+
+if (process.argv[2] === '--dev') {
+  ctx.then(ctx => {
+    ctx.rebuild().then(() => {
+      startNode();
+
+    });
+  })
+} else {
+  ctx.then(ctx => {
+    ctx.rebuild();
+    ctx.dispose();
+  });
+}
