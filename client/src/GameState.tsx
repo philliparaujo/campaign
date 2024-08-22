608705b286867acc8418bb516ec277d9fab9a8c4
Phillip Araujo <67209209+philliparaujo@users.noreply.github.com>
2024-08-21 22:16:55 -0400

    Reverted maxTurns to 3

--- client/src/GameState.tsx
+++ client/src/GameState.tsx
@@ -34,7 +34,7 @@ export const size = 5;
 export const maxRoadsAllowed = 15; // ~ 3*size is best
 
 export const startingCoins = 10;
-export const maxTurns = 1;
+export const maxTurns = 3;
 
 const initialPlayer: PlayerInfo = {
   id: '',
